c16a32aac3f7bcbfcb4f78dfc1ac91cd4ddb0f46
FahriGans12 <102817419+FahriGans12@users.noreply.github.com>
2023-01-20 18:11:00 +0700

    Add files via upload

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -1,31 +1,21 @@
-import fs from 'fs'
+import { sticker } from '../lib/sticker.js'
 
-import fetch from 'node-fetch'
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-let handler  = async (m, { conn, usedPrefix: _p }) => {
-
-let info = `🐋`
-
-
-
-let td = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-
-
-conn.reply(m.chat, info, m, { quoted: fakes },{ contextInfo: { externalAdReply: { showAdAttribution: true,
-      mediaUrl: "https://Instagram.com/Fdev90",
-      mediaType: 2,
-      description: "https://Instagram.com/Fdev90", 
-      title: global.titlebot,
-      body: wm,
-      thumbnail: thumb,
-      sourceUrl: sig  }}})
-
+let stiker = await sticker(null, global.API(`${pickRandom(stikerhuuu)}`), global.packname, global.author)
+    if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+    throw stiker.toString()
+    
 }
 
-handler.customPrefix = /^(tes|bot|p|test)$/i
-
+handler.customPrefix = /^(woi|halo|hallo|test|tes|p|cok)$/i
 handler.command = new RegExp
 
-
-
 export default handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+let stikerhuuu = [
+ "https://telegra.ph/file/d662e1de92db7feb0ad39.png",
+]
